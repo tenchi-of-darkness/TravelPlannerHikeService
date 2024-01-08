7fbc5ed22c305a878120f17b57dda32e1fbdcb1a
Melanie <melanievandegraaf1@live.nl>
2024-01-08 20:17:02 +0100

    Fixed hub method name not right

--- Hike.API/Hubs/MapHub.cs
+++ Hike.API/Hubs/MapHub.cs
@@ -6,6 +6,6 @@ public class MapHub : Hub
 {
     public async Task SendLocation(string userId, string userName, double latitude, double longitude)
     {
-        await Clients.All.SendAsync("Receive Location", userId, userName, latitude, longitude);
+        await Clients.All.SendAsync("ReceiveLocation", userId, userName, latitude, longitude);
     }
 }
